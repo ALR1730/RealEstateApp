26b946fb31e3e830e1b80482627f0c32f310d5bd
Angel Luis R <angelluisrosario12345@gmail.com>
2026-07-27 10:33:30 -0400

    chore: build project artifacts and generate debug output files

--- src/Core/RealEstateApp.Core.Application/obj/Debug/net10.0/RealEstateApp.Core.Application.AssemblyInfo.cs
+++ src/Core/RealEstateApp.Core.Application/obj/Debug/net10.0/RealEstateApp.Core.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateApp.Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a17d127bf7851052e530b45507fed9ffe56d185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2636da849af672789e698304c910e05d2be31ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateApp.Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateApp.Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
